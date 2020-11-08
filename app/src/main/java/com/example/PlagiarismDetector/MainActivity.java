b88cfdfb45303e4edf05cc0f1e7b75eb061c5917
Menna Khaliel <56710050+Menna13@users.noreply.github.com>
2020-11-08 01:32:56 -0500

    edits after text extraction

--- app/src/main/java/com/example/PlagiarismDetector/MainActivity.java
+++ app/src/main/java/com/example/PlagiarismDetector/MainActivity.java
@@ -15,19 +15,28 @@ import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 import static androidx.documentfile.provider.DocumentFile.fromSingleUri;
 
 public class MainActivity extends AppCompatActivity {
     Button btnAttach;
+    TextView tvText;
     Context context;
     private static final int PICK_PDF_FILE = 2;
     @Override
@@ -42,30 +51,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                openFile();
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("application/*");
-//        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
                 startActivityForResult(intent, PICK_PDF_FILE);
                 setResult(Activity.RESULT_OK, intent);
             }
         });
     }
 
-
-//    private void openFile (){
-//        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
-//        intent.setType("application/msword");
-////        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
-//        startActivityForResult(intent, PICK_PDF_FILE);
-//        setResult(Activity.RESULT_OK, intent);
-//        Uri data = intent.getData();
-//        System.out.println("see here");
-//        System.out.println(data);
-//        onActivityResult(data);
-//
-//    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         super.onActivityResult(requestCode, resultCode, resultData);
@@ -78,10 +72,11 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("see here");
                 System.out.println(uri);
                 Log.d("MainActivity", "ReachedOnActivityResultIF");
-                //                AccessFile(uri, this.context);
-                //                DocumentFile documentFile = fromSingleUri(context, uri);
                 String uriString = uri.toString();
+                System.out.println(uriString);
                 File documentFile = new File(uriString);
+                System.out.println(documentFile.canRead());
+                System.out.println(documentFile.getName());
                 String path = documentFile.getAbsolutePath();
                 String displayName = null;
                 if (uriString.startsWith("content://")) {
@@ -89,15 +84,20 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         cursor = getContentResolver().query(uri, null, null, null, null);
                         if (cursor != null && cursor.moveToFirst()) {
+                            System.out.println("got into content");
                             displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                            AccessFile(displayName, documentFile);
+                            readTextFromUri(uri);
+
                         }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     } finally {
                         cursor.close();
                     }
                 } else if (uriString.startsWith("file://")) {
+                    System.out.println("got into file");
                     displayName = documentFile.getName();
-                    AccessFile(displayName, documentFile);
+
                 }
             }
         }
@@ -105,15 +105,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void AccessFile(String filename, File document) {
+    private String readTextFromUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream =
+                     getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append('\n');
+            }
+        }
+        tvText = findViewById(R.id.tvExtracted);
+        tvText.setText(stringBuilder);
+        return stringBuilder.toString();
+
+
+    }
+
+
+    public void AccessFile(String filename, File document, InputStream inputStream) {
         Log.d("MainActivity", "ReachedAccessFile");
-//        DocumentFile documentFile = fromSingleUri(context, uri);
-//        documentFile.getName();
         try {
             Log.d("MainActivity", "ReachedAcessFileTRY");
             //change uri to file name and get it from document file
-            InputStream fis = new FileInputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + filename);
-            XWPFDocument xdoc = new XWPFDocument(OPCPackage.open(fis));
+//            InputStream fis = new FileInputStream(document);
+
+            XWPFDocument xdoc = new XWPFDocument(OPCPackage.open(inputStream));
             XWPFWordExtractor extractor = new XWPFWordExtractor(xdoc);
             System.out.println("maybe parsed?");
             System.out.println(extractor.getText());
@@ -121,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
 
+
     }
 
 
@@ -128,64 +148,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-//        public void TextReader() {
-//            try {
-//                InputStream fis = new FileInputStream("test.docx");
-//                XWPFDocument xdoc = new XWPFDocument(OPCPackage.open(fis));
-//                XWPFWordExtractor extractor = new XWPFWordExtractor(xdoc);
-//                System.out.println(extractor.getText());
-//            } catch(Exception ex) {
-//                ex.printStackTrace();
-//            }
-//        }
-
-
-
-        /*    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        super.onActivityResult(requestCode, resultCode, resultData);
-        if (requestCode == PICK_PDF_FILE
-                && resultCode == Activity.RESULT_OK) {
-            Log.d("MainActivity", "ReachedOnActivityResult");
-            // The result data contains a URI for the document or directory that
-            // the user selected.
-            Uri uri = null;
-            if (resultData != null) {
-                uri = resultData.getData();
-                Log.d("MainActivity", "ReachedOnActivityResultIF");
-                // Perform operations on the document using its URI.
-            }
-        }
-    }*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- /*   public void attachButton_OnClick(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    private void getDocument()
-    {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("application/msword,application/pdf");
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        // Only the system receives the ACTION_OPEN_DOCUMENT, so no need to test.
-//        startActivityForResult(intent, REQUEST_CODE_DOC);
-    }
-
+/*
 
     @Override
     protected void onActivityResult(int req, int result, Intent data)
